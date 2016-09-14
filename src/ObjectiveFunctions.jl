aaefd4386dead12fb2e9fc7b9fb434e045e99f8a
Thomas Breloff <tom@breloff.com>
2016-09-14 17:31:00 -0400

    CrossEntropy; objective

--- src/ObjectiveFunctions.jl
+++ src/ObjectiveFunctions.jl
@@ -12,14 +12,25 @@ import LearnBase: transform!, grad, grad!
 import Transformations: input_node, output_node, params
 
 export
+    AbstractLossTransform,
     LossTransform,
-    RegularizedObjective
+    CrossEntropy,
+    RegularizedObjective,
+    objective
+
+abstract AbstractLossTransform{T} <: Transformation
+
+# this assumes that input_value(lt) is pre-populated... copy the target in, then pass to no-arg version
+function transform!(lt::AbstractLossTransform, target::AbstractVector)
+    copy!(value(lt.target), target)
+    transform!(lt)
+end
 
 # ------------------------------------------------------------------------
 
 # this is a transformation which stores the calculated loss, as well as the gradients of the inputs
 # it can be linked to a transformation to allow easier forward/backward calcs
-immutable LossTransform{T,L<:Loss} <: Transformation
+immutable LossTransform{T,L<:Loss} <: AbstractLossTransform{T}
     loss::L
     nin::Int
     input::Node{:input,T,1}
@@ -35,12 +46,6 @@ immutable LossTransform{T,L<:Loss} <: Transformation
     end
 end
 
-# this assumes that input_value(lt) is pre-populated... copy the target in, then pass to no-arg version
-function transform!(lt::LossTransform, target::AbstractVector)
-    copy!(value(lt.target), target)
-    transform!(lt)
-end
-
 # input and target are pre-populated... compute the output value as: ∑ loss(targetᵢ, inputᵢ)
 function transform!(lt::LossTransform)
     lt.output.val[1] = sumvalue(lt.loss, value(lt.target), input_value(lt))
@@ -53,25 +58,61 @@ function grad!(lt::LossTransform)
     deriv!(grad(lt.input), lt.loss, value(lt.target), value(lt.input))
 end
 
+# ------------------------------------------------------------------------
+
+# NOTE: special handling for the Softmax/CrossEntropyLoss layer.
+#   Instead of a proper activation function layer, we do both the
+#   softmax activation and cross entropy loss together.
+
+immutable CrossEntropy{T} <: AbstractLossTransform{T}
+    n::Int
+    input::Node{:input,T,1}
+    target::Node{:target,T,1}
+    output::Node{:output,T,1}
+
+    function CrossEntropy(n::Int)
+        input = Node(:input, zeros(T, n))
+        target = Node(:target, zeros(T, n))
+        output = Node(:output, zeros(T, 1))
+        grad(output)[:] = one(T)  # ∂L/∂L == 1
+        new(n, input, target, output)
+    end
+end
+CrossEntropy(n::Int) = CrossEntropy{Float64}(n)
+
+function transform!{T}(ce::CrossEntropy{T})
+    ce.output.val[1] = -sum(ce.target.val[i] * log(ce.input.val[i]) for i=1:ce.n)
+end
+
+# ** NOTE **: This is really solving for the gradient with respect to the softmax activation inputs,
+#             but we diverge from correctness to allow for the simplified gradient calc.
+function grad!{T}(ce::CrossEntropy{T})
+    for i=1:ce.n
+        ce.input.∇[i] = ce.input.val[i] - ce.target.val[i]
+    end
+end
 
 # ------------------------------------------------------------------------
 
 # TODO: should we support penalties on outputs as well as parameters?
 
 # convenience type for Empirical Risk Minimization and similar
-type RegularizedObjective{T<:Transformation, L<:LossTransform, P<:Penalty} <: Minimizable
+immutable RegularizedObjective{T<:Transformation, L<:AbstractLossTransform, P<:Penalty} <: Minimizable
     transformation::T
     loss::L
     penalty::P
 end
 
-function RegularizedObjective(transformation::Transformation, loss::Loss, penalty::Penalty = NoPenalty())
-    # create a LossTransform and link it to the transformation output
-    lt = LossTransform{Float64,typeof(loss)}(loss, output_length(transformation))
+# first link the transformation to the loss, then construct
+function objective(transformation::Transformation, lt::AbstractLossTransform, penalty::Penalty = NoPenalty())
     link_nodes!(output_node(transformation), input_node(lt))
-
-    # return the constructed object
     RegularizedObjective(transformation, lt, penalty)
+end
+
+# first create the LossTransform to wrap the Loss, then construct
+function objective(transformation::Transformation, loss::Loss, penalty::Penalty = NoPenalty())
+    lt = LossTransform{Float64,typeof(loss)}(loss, output_length(transformation))
+    objective(transformation, lt, penalty)
 end
 
 input_node(obj::RegularizedObjective) = input_node(obj.transformation)
